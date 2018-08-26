658881bfd4cce0e6ece1e7f6b2a04587a6d4bb4e
uni <beachside5088@gmail.com>
2018-08-26 12:10:17 +0900

    login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tops#home'
 
   get '/contact',       to: 'tops#contact'
@@ -6,5 +7,9 @@ Rails.application.routes.draw do
   get 'terms_of_use',   to: 'tops#terms_of_use'
   get '/signup',        to: 'users#new'
   post '/signup',       to: 'users#create'
+  get '/login',         to: 'sessions#new'
+  post '/login',        to: 'sessions#create'
+  delete '/logout',     to: 'sessions#destroy'
+
   resources :users
 end
