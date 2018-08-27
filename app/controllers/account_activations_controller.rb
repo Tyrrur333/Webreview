e2e3362c98e36cf3984f8c540607a08e6f602ad9
uni <beachside5088@gmail.com>
2018-08-27 18:46:52 +0900

    add account-activation

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,14 @@
+class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activated
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "アカウントが有効化されました"
+      redirect_to user
+    else
+      flash[:danger] = "無効なアカウント有効化のリンクです"
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "新規登録に成功しました！"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "メールアドレスを確認してアカウントの有効化をしてください"
+      redirect_to root_url
     else
       render 'new'
     end
