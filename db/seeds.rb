ce7e9857e6780bea86ac5ef7107a47d882646904
uni <beachside5088@gmail.com>
2018-09-16 16:38:27 +0900

    add seed image

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
+             image: "default.jpg",
              activated: true,
              activated_at: Time.zone.now)
 
@@ -13,6 +14,7 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
+               image: "default.jpg",
                activated: true,
                activated_at: Time.zone.now)
 end
